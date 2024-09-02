1ad1b0bbd850a81b713f44716ab2688346f57853
Han Yoon Soo <hanhs9803753@gmail.com>
2024-09-02 10:59:30 +0900

    Test : Accounts System

--- src/main/java/com/example/test/accounts/AccountsController.java
+++ src/main/java/com/example/test/accounts/AccountsController.java
@@ -1,15 +1,32 @@
 package com.example.test.accounts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@Controller @RestController
 public class AccountsController {
+    @Autowired
+    AccountsServiceImp service;
+
     @GetMapping("/accounts/sign")
     public String sign(Model model) {
         return "accounts/sign";
     }
-    
+    @GetMapping("/accounts/forgot-password")
+    public String forgotPassword(Model model) {
+        return "accounts/forgot-password";
+    }
+    @GetMapping("/accounts/reset-password")
+    public String resetPassword(Model model) {
+        return "accounts/reset-password";
+    }
+    @GetMapping("/accounts/confirm-mail")
+    public String confirmMail(@RequestParam Long id) {
+        return "accounts/confirm-mail";
+    }
 }
---
+++ src/main/java/com/example/test/accounts/AccountsServiceImp.java
@@ -0,0 +1,8 @@
+package com.example.test.accounts;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountsServiceImp implements AccountsService {
+    
+}

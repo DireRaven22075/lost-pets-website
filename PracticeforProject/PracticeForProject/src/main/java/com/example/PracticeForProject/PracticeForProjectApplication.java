f28bb01951c9f40bf27ca71018542ff1139421f4
C0BlA <cobia9e@gmail.com>
2024-11-05 23:22:09 +0900

    practice

---
+++ PracticeforProject/PracticeForProject/src/main/java/com/example/PracticeForProject/PracticeForProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.PracticeForProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeForProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeForProjectApplication.class, args);
+	}
+
+}
---
+++ PracticeforProject/PracticeForProject/src/test/java/com/example/PracticeForProject/PracticeForProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PracticeForProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeForProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

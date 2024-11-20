db22d162da528da4985c9c249aba91a3b3a00068
yaho821 <yecongE0821@gmail.com>
2024-11-20 11:45:52 +0900

    11/20
    setting

--- src/main/java/team/ccnu/project/controller/ViewController.java
+++ src/main/java/team/ccnu/project/controller/ViewController.java
@@ -1,9 +1,9 @@
 package team.ccnu.project.controller;
 
-import org.json.HTTP;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,7 +26,7 @@ public class ViewController {
     ///</summary>
     @GetMapping
     public ModelAndView index(HttpServletRequest request, Model model) {
-        ModelAndView mav = buildModelAndView(request, "__TEST__/index");
+        ModelAndView mav = buildModelAndView(request, "index");
         return mav;
     }
     ///<summary>
@@ -35,7 +35,7 @@ public class ViewController {
     ///</summary>
     @GetMapping("/login")
     public ModelAndView login(HttpServletRequest request, Model model) {
-        ModelAndView mav = buildModelAndView(request, "__TEST__/login");
+        ModelAndView mav = buildModelAndView(request, "login");
         return mav;
     }
     ///<summary>
@@ -44,7 +44,7 @@ public class ViewController {
     ///</summary>
     @GetMapping("/register")
     public ModelAndView register(HttpServletRequest request) {
-        ModelAndView mav = buildModelAndView(request,"__TEST__/register");
+        ModelAndView mav = buildModelAndView(request,"register");
         return mav;
     }
     ///<summary>
@@ -53,60 +53,60 @@ public class ViewController {
     ///</summary>
     @GetMapping("/post")
     public ModelAndView testPost(HttpServletRequest request) {
-        ModelAndView mav = buildModelAndView(request,"__TEST__/post");
+        ModelAndView mav = buildModelAndView(request,"post");
         return mav;
     }
-//    ///<summary>
-//    /// [HTTP GET] 비밀번호 착기 화면 출력 함수
-//    /// TODO : 구현 필요
-//    ///</summary>
-//    @GetMapping("/forgot-password")
-//    public ModelAndView forgotPassword(HttpServletRequest request) {
-//        ModelAndView mav = buildModelAndView(request,"/forgot-password");
-//        return mav;
-//    }
-//    ///<summary>
-//    /// [HTTP GET] 회원 정보 화면 출력 함수
-//    /// TODO : 구현 필요
-//    ///</summary>
-//    @GetMapping("/ami")
-//    public ModelAndView ami(Model model) {
-//        ModelAndView mav = buildModelAndView("/ami");
-//        return mav;
-//    }
-//    ///<summary>
-//    /// [HTTP GET] 게시판 화면 출력 함수
-//    /// TODO : 구현 필요
-//    ///</summary>
-//    @GetMapping("/board/{id}/{index}")
-//    public ModelAndView board(Model model, @PathVariable String id, @PathVariable String index) {
-//        ModelAndView mav = buildModelAndView("/board");
-//        return mav;
-//    }
-//    /// <summary>
-//    /// [HTTP GET] 게시글 화면 출력 함수
-//    /// TODO : 구현 필요
-//    /// </summary>
-//    @GetMapping("/post/{id}/{sn}")
-//    public ModelAndView post(Model model, @PathVariable String id, @PathVariable String sn) {
-//        ModelAndView mav = buildModelAndView("/board");
-//        return mav;
-//    }
-//    ///<summary>
-//    /// [HTTP GET] 게시글 작성 화면 출력 함수
-//    /// TODO : 구현 필요
-//    ///</summary>
-//    @GetMapping("/write/{id}")
-//    public ModelAndView write(Model model, @PathVariable String id) {
-//        ModelAndView mav = buildModelAndView("/board");
-//        return mav;
-//    }
-//    ///<summary>
-//    /// [HTTP GET] 게시글
-//    ///</summary>
-//    @GetMapping("/modify/{id}/{uid}")
-//    public ModelAndView modify(Model model, @PathVariable String id, @PathVariable String uid) {
-//        ModelAndView mav = buildModelAndView("/board");
-//        return mav;
-//    }
+   ///<summary>
+   /// [HTTP GET] 비밀번호 착기 화면 출력 함수
+   /// TODO : 구현 필요
+   ///</summary>
+   @GetMapping("/forgot-password")
+   public ModelAndView forgotPassword(HttpServletRequest request) {
+       ModelAndView mav = buildModelAndView(request,"/forgot-password");
+       return mav;
+   }
+   ///<summary>
+   /// [HTTP GET] 회원 정보 화면 출력 함수
+   /// TODO : 구현 필요
+   ///</summary>
+   @GetMapping("/ami")
+   public ModelAndView ami(HttpServletRequest request) {
+       ModelAndView mav = buildModelAndView(request, "/ami");
+       return mav;
+   }
+   ///<summary>
+   /// [HTTP GET] 게시판 화면 출력 함수
+   /// TODO : 구현 필요
+   ///</summary>
+   @GetMapping("/board/{id}/{index}")
+   public ModelAndView board(HttpServletRequest request, @PathVariable String id, @PathVariable String index) {
+       ModelAndView mav = buildModelAndView(request, "/board");
+       return mav;
+   }
+   /// <summary>
+   /// [HTTP GET] 게시글 화면 출력 함수
+   /// TODO : 구현 필요
+   /// </summary>
+   @GetMapping("/post/{id}/{sn}")
+   public ModelAndView post(HttpServletRequest request, @PathVariable String id, @PathVariable String sn) {
+       ModelAndView mav = buildModelAndView(request, "/board");
+       return mav;
+   }
+   ///<summary>
+   /// [HTTP GET] 게시글 작성 화면 출력 함수
+   /// TODO : 구현 필요
+   ///</summary>
+   @GetMapping("/write/{id}")
+   public ModelAndView write(HttpServletRequest request, @PathVariable String id) {
+       ModelAndView mav = buildModelAndView(request, "/board");
+       return mav;
+   }
+   ///<summary>
+   /// [HTTP GET] 게시글
+   ///</summary>
+   @GetMapping("/modify/{id}/{uid}")
+   public ModelAndView modify(HttpServletRequest request, @PathVariable String id, @PathVariable String uid) {
+       ModelAndView mav = buildModelAndView(request, "/board");
+       return mav;
+   }
 }

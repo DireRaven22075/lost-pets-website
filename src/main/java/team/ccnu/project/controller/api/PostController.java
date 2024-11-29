0b455698ec75d8a3214198bd29c28e2c1a42be42
박상인 <cobia9e@gmail.com>
2024-11-29 15:09:06 +0900

    fix:jsontype

--- src/main/java/team/ccnu/project/controller/api/PostController.java
+++ src/main/java/team/ccnu/project/controller/api/PostController.java
@@ -3,12 +3,18 @@ package team.ccnu.project.controller.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import team.ccnu.project.data.request.UploadPostDTO;
-import team.ccnu.project.data.response.PostDTO;
 import team.ccnu.project.domain.entity.Post;
 import team.ccnu.project.service.PostService;
+
 
 @RestController
 @RequestMapping("/api/posts")
@@ -29,8 +35,9 @@ public class PostController {
 
             postService.savePost(post);
 
-            PostDTO responseDTO = new PostDTO(post);
-            return ResponseEntity.ok().body(responseDTO);
+            return ResponseEntity.ok().body("""
+                    {"status": "success", "message": "Post created successfully"}
+                    """);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("""
                     {"status": "error", "message": "Internal Server Error"}
@@ -39,6 +46,8 @@ public class PostController {
     }
 
     /// <게시글 삭제>
+    /// api -dto를 return하는 것이 아니라, responseEntity를 return하는 것이 맞다.
+    /// 계정이 있는 경우에만 삭제 할 수 있도록 
     @DeleteMapping("/{postSn}")
     public ResponseEntity<?> apiDeletePost(@PathVariable Long postSn) {
         try {
@@ -58,10 +67,9 @@ public class PostController {
     public ResponseEntity<?> apiUpdatePost(@PathVariable Long postSn, @RequestBody UploadPostDTO postDTO) {
         try {
             Post post = postService.updatePost(postSn, postDTO);
-            PostDTO responseDTO = new PostDTO(post);
             return ResponseEntity.ok().body("""
-                    {"status": "success", "data": %s}
-                    """.formatted(responseDTO));
+                    {"status": "success", "message": "Post updated successfully"}
+                    """);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("""
                     {"status": "error", "message": "Internal Server Error"}
